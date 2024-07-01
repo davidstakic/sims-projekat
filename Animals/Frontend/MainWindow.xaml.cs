c2bcafa4be70fbbe6ca97e96d6819ce541cceecb
vzdravkovic7 <141317094+vzdravkovic7@users.noreply.github.com>
2024-07-01 13:34:27 +0200

    [ADD] Login Window Design and Registration

--- Animals/Frontend/MainWindow.xaml.cs
+++ Animals/Frontend/MainWindow.xaml.cs
@@ -1,24 +1,50 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Frontend
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        //private ProfilesController _profilesController { get; set; }
+        //private StudentsController _studentsController { get; set; }
+        //private ProfessorsController _professorsController { get; set; }
+        //private ProfessorGradesController _professorGradesController { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            //new Database().CreateAndPopulateTables();
+            //_profilesController = new ProfilesController();
+            //_studentsController = new StudentsController();
+            //_studentsController.ReducePenaltyPoints(_profilesController);
+            //_professorsController = new ProfessorsController();
+            //_professorGradesController = new ProfessorGradesController();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            //new LoginMenu(_studentsController, _professorsController, _professorGradesController).Show();
+            string email = EmailTextBox.Text;
+            string password = PasswordBox.Password;
+            MessageBox.Show($"Attempting to log in with Email: {email} and Password: {password}");
+            Close();
+        }
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordText.Visibility = string.IsNullOrEmpty(PasswordBox.Password) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        private void ForgotPassword_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Forgot Password clicked!");
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            //new RegistrationMenu(_studentsController).Show();
+            Close();
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
