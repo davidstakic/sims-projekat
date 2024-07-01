b802449b0e2a4052c7c0ce7bf90b77c23ffd093d
David Stakic <davidstakic2003@gmail.com>
2024-07-01 10:52:22 +0200

    [FEATURE] All Services Implemented

---
+++ Animals/Backend/Services/AnimalServices/AnimalService.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Configuration;
+using Backend.Models.AnimalModels;
+using Backend.Repositories.Interfaces.AnimalInterfaces;
+
+namespace Backend.Services.AnimalServices
+{
+    public class AnimalService : Service<Animal>
+    {
+        public AnimalService() : base((IAnimalRepository)Injector.GetRepositoryInstance("IAnimalRepository"))
+        {
+        }
+    }
+}
