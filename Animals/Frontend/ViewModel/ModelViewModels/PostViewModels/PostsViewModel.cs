7db38ec32312b88f56c916701a2590ee3fa50980
Marko Cvijanović <markocvijanovicmlg@gmail.com>
2024-07-02 00:22:16 +0200

    [ADD] Individual Post Display

--- Animals/Frontend/ViewModel/ModelViewModels/PostViewModels/PostsViewModel.cs
+++ Animals/Frontend/ViewModel/ModelViewModels/PostViewModels/PostsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Backend.Models.PostModels;
+using Backend.Models.AnimalModels;
+using Backend.Models.UserModels;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
@@ -24,12 +26,17 @@ public class PostsViewModel : INotifyPropertyChanged
         var postsData = LoadDataFromFile<Post>("../../../../Backend/Data/Posts.json");
         var likesData = LoadDataFromFile<Like>("../../../../Backend/Data/Likes.json");
         var commentsData = LoadDataFromFile<Comment>("../../../../Backend/Data/Comments.json");
-        var memberData = LoadDataFromFile<Comment>("../../../../Backend/Data/Members.json");
+        var memberData = LoadDataFromFile<Member>("../../../../Backend/Data/Members.json");
+        var animalsData = LoadDataFromFile<Animal>("../../../../Backend/Data/Animals.json");
+        var speciesData = LoadDataFromFile<AnimalSpecie>("../../../../Backend/Data/Species.json");
 
         var postsWithDetails = from post in postsData
                                join like in likesData on post.Id equals like.PostId into postLikes
                                join comment in commentsData on post.Id equals comment.PostId into postComments
-                               select new PostViewModel(post, postLikes.Count(), postComments.Count());
+                               join member in memberData on post.UserId equals member.Id
+                               join animal in animalsData on post.AnimalId equals animal.Id
+                               join specie in speciesData on animal.AnimalSpecieId equals specie.Id
+                               select new PostViewModel(post, member.FirstName, member.LastName, animal, specie.Name, postLikes.Count(), postComments.Count());
 
         Posts = new ObservableCollection<PostViewModel>(postsWithDetails);
     }
